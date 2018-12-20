743878872d437c7ed91ef6af684d27a0bc334c9d
Celyn Susanto <celyn.vy@gmail.com>
2018-12-20 18:24:23 +0700

    add playlist

--- routes/playlist.js
+++ routes/playlist.js
@@ -2,6 +2,21 @@ const express = require('express')
 const router = express.Router()
 const Model = require('../models')
 
+router.get('/:userId', (req, res) => {
+    res.send(req.session)
+    // Model.Playlist
+    // .findAll({where: {
+    //     id: req.session.user.id
+    // }})
+    // .then((playlist) => {
+    //     res.send(playlist)
+    // })
+    // res.render('./pages/playlist')
+})
+
+router.get('/:userId/createPlaylist', (req, res) => {
+    res.render()
+})
 
 
 module.exports = router
